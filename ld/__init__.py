39b36a0d6bccf675367add7b57b11bcaf2b64ff1
knowhw <142631271+knowhw@users.noreply.github.com>
2024-04-07 19:30:50 -0500

    Update __init__.py

--- ld/__init__.py
+++ ld/__init__.py
@@ -1,22 +1,15 @@
 
 from ld import glob
 
-
 def applications():
-
+	
 	import os
-	
-	
-	# global path_desktop_items
-	path_desktop_items = [ os.path.realpath(item) 
-	for item in os.listdir(glob.appdir) ]
-
 
 	dictionary = {}
-	""" dictionary: app list => usr share applications """
+	""" dictionary: app list => usr/share/applications """
 	
 	
-	for item in path_desktop_items:
+	for item in [ os.path.realpath(item) for item in os.listdir(glob.appdir) ]:
 		
 		
 		name, extension = os.path.splitext(os.path.basename(item))
