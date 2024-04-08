8249405ed4c7859659f33715c1bc527b1713937a
knowhw <142631271+knowhw@users.noreply.github.com>
2024-04-07 19:32:40 -0500

    Update __init__.py

--- ld/__init__.py
+++ ld/__init__.py
@@ -2,24 +2,18 @@
 from ld import glob
 
 def applications():
-	
-	import os
 
-	dictionary = {}
-	""" dictionary: app list => usr/share/applications """
-	
-	
-	for item in [ os.path.realpath(item) for item in os.listdir(glob.appdir) ]:
+	for item in [ glob.os.path.realpath(item) for item in glob.os.listdir(glob.appdir) ]:
 		
 		
-		name, extension = os.path.splitext(os.path.basename(item))
+		name, extension = glob.os.path.splitext(glob.os.path.basename(item))
 		if ".desktop" != extension:
 			continue
 		
-		dictionary [name] = '%s/%s%s' % (glob.appdir, name, extension)
+		glob.dictionary [name] = '%s/%s%s' % (glob.appdir, name, extension)
 		""" 'path':'%s/%s%s' % (glob.appdir, exec, extension) """
 		
 	
-	return dictionary
+	return glob.dictionary
 
 
