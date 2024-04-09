6689f47cb2ffe31689873737199f284025088e73
knowhw <142631271+knowhw@users.noreply.github.com>
2024-04-08 19:21:39 -0500

    Add files via upload

--- start.sh
+++ start.sh
@@ -6,3 +6,7 @@ sudo systemctl enable --now dc-launcher.service
 # sudo systemctl restart dc-launcher.service
 
 sudo systemctl status dc-launcher.service
+
+#
+sudo apt autoremove deepin-picker kazam -y
+sudo apt install deepin-picker kazam -y
